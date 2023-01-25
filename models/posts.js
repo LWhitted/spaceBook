469ca3a98dded265843bbbc9f252bba7bafb0964
Pat-Link <Patlink1003@yahoo.com>
2023-01-24 19:57:01 -0600

    notes on models and server connection to render

--- models/posts.js
+++ models/posts.js
@@ -22,3 +22,5 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Posts;
 };
+
+//add username
---
+++ server.js
@@ -0,0 +1,2 @@
+const sequelize = require('sequelize')
+const sequelize = new Sequelize('postgres://spacebook_user:ir6kwf7pelzKwZxFvWE9Pa24jCKGpIQI@dpg-cf87rq6n6mplr41b2et0-a/spacebook')
